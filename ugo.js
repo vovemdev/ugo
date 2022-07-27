d6619ee363ed69f30f42ef3adf8dca6d434adc75
OgunniyiOwamamwen <owamamwen@outlook.com>
2022-07-27 10:33:17 +0100

    'functionalities'

--- ugo.js
+++ ugo.js
@@ -29,6 +29,10 @@ function Ugo (){
     Ugo.prototype.divide = function (a, b) {
     return a/b;
     }
+    // modulus operation (remainder) values of integer
+    Ugo.prototype.modulo = function mod (a, b){
+        return ((a % b) + b) % b;
+    }
     // Flatten
     Ugo.prototype.flatten = function flat (obj) {
         const resultArr = [];
@@ -41,5 +45,34 @@ function Ugo (){
         });
         return resultArr;
     };
+        // Even array values of integer
+        Ugo.prototype.evenArray = function even (obj) {
+            const resultEven = [];
+          obj.forEach(n => {
+            if(n % 2 === 0){
+                resultEven.push(n);
+            }
+          })
+            return resultEven;
+        };
+        // Odd array values of integer
+        Ugo.prototype.oddArray = function odd (obj) {
+            const resultOdd = obj.filter(n => {
+              return n % 2 != 0;
+            })
+              return resultOdd;
+          };
+        // Exponential values of integer
+        Ugo.prototype.exponent = function expo (a, b){
+            let e = 1;
+            for(let i = 1; i <= b; i++) {
+                e *= a;
+            }
+            return e;
+        }
+        //Power values of integer
+        Ugo.prototype.power = function pow (b, e){
+            return e == 0? 1 : b * this.power(b, --e);
+        }
 
     export default Ugo;
