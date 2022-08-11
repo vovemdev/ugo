2a178077117f8758ea60c46bc25af104f2c49747
Ogunniyi Owamamwen.F <ogunniyiowamamwen@gmail.com>
2022-08-12 00:22:50 +0200

    Update fibonacciGenerator.js

--- src/shared/fibonacciGenerator.js
+++ src/shared/fibonacciGenerator.js
@@ -3,12 +3,12 @@ vovem-ugo v2.4.1 | Ugo is mathematics an open-source library for JavaScript comm
 Copyright 8/6/2022, 21:10:13 PM | VovemDev | MIT license 
 */
 
-const arithmetic = require('../utils/arithmetic');
+// const arithmetic = require('../utils/arithmetic');
 
 // Fibonacci series generator
 // F(n) = F(n-1) + F(n-2)
 module.exports = function fibonacciGenerator (n) {
-    const arr = new Array(n).fill(1).arithmetic((f,_,i) => {
+    const arr = new Array(n).fill(1).reduce((f,_,i) => {
         f.push((i <= 1) ? i : f[i-2] + f[i-1]);
         return f;
     }, []);
