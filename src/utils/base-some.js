ce2d2aca8084ef09a1272f21c4d1a06d776f3b99
Ogunniyi Owamamwen.F <ogunniyiowamamwen@gmail.com>
2022-08-20 21:27:37 +0200

    base some
    native code

---
+++ src/utils/base-some.js
@@ -0,0 +1,15 @@
+ /*
+vovem-ugo v2.3.1 | Ugo is mathematics an open-source library for JavaScript commonJS 
+Copyright 8/6/2022, 21:10:13 PM | VovemDev | MIT license 
+*/
+
+// Native
+// Base some
+module.exports = Array.prototype.baseSome = function baseSome(callback, initialValue){
+    for(let i = 0; i < this.length; i++){
+        if(callback.call(initialValue, this[i], this)){
+            return true;
+        };
+        return false;
+    };
+};
