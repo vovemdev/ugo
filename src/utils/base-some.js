521e0d0f74d806ece5fa66c77a807e10a0a1fa0a
Ogunniyi Owamamwen.F <ogunniyiowamamwen@gmail.com>
2022-08-23 00:04:46 +0200

    Update base-some.js
    refactory

--- src/utils/base-some.js
+++ src/utils/base-some.js
@@ -3,7 +3,6 @@ vovem-ugo v2.3.1 | Ugo is mathematics an open-source library for JavaScript comm
 Copyright 8/6/2022, 21:10:13 PM | VovemDev | MIT license 
 */
 
-// Native
 // Base some
 module.exports = Array.prototype.baseSome = function baseSome(callback, initialValue){
     for(let i = 0; i < this.length; i++){
