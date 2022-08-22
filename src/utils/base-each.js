6adca2e7e52e39933331ae7bfa58cd456769c1f0
Ogunniyi Owamamwen.F <ogunniyiowamamwen@gmail.com>
2022-08-23 00:03:14 +0200

    Update base-each.js
    remove

--- src/utils/base-each.js
+++ src/utils/base-each.js
@@ -1,9 +1,8 @@
-/*! 
+/*
 vovem-ugo v2.3.1 | Ugo is mathematics an open-source library for JavaScript commonJS 
 Copyright 8/6/2022, 21:10:13 PM | VovemDev | MIT license 
 */
 
-// Native 
 // Base each
 module.exports = Array.prototype.baseEach = function (initialValue){
     for(let i = 0; i< this.length; i++){
